278177962ff54cbd35716eb7bfdc3494fb51babf
Frostie314159 <frostie.neuenhausen@gmail.com>
2025-03-14 14:35:39 +0100

    Added minimal example template.

---
+++ examples/src/bin/minimal.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+use embassy_executor::Spawner;
+use esp_backtrace as _;
+use esp_hal::timer::timg::TimerGroup;
+use esp_wifi_hal::{DMAResources, WiFi};
+
+#[esp_hal_embassy::main]
+async fn main(_spawner: Spawner) {
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    esp_println::logger::init_logger_from_env();
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_hal_embassy::init(timg0.timer0);
+    let mut dma_resources = DMAResources::<10>::new();
+    let _wifi = WiFi::new(
+        peripherals.WIFI,
+        peripherals.RADIO_CLK,
+        peripherals.ADC2,
+        &mut dma_resources,
+    );
+}
